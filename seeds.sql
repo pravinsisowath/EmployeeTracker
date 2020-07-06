f9335779cf3c693fbeb94aac9bc07df66215fdef
Pravin Sisowath <pravinsisowath@gmail.com>
2020-07-06 16:54:42 -0700

    seed

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+USE employeedb,
+
+INSERT INTO employee (firstName, lastName)
+VALUES 
+    ("Pravin", "Sisowath")
+
+INSERT INTO role (role)
+VALUES 
+    ("Employee")
+
+INSERT INTO department (name)
+VALUES 
+    ("HR")
+
+
+
+
